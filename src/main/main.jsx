816975132d706416eef629e07e2ee77c1619d2ab
Chaldrak <chaldrakus@gmail.com>
2022-11-14 23:38:55 +0100

    default route

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from '../pages/App'
 import Header from '../components/header/Header'
 import './index.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Container from '../components/container/Container';
 import Stats from '../components/container/Stats';
 
@@ -12,7 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<App />}>
+        <Route path='/' element={<App />} >
+          <Route path='' element={<Navigate to="/countries" replace="true" />} />
           <Route path='/countries' element={<Container />} />
           <Route path='/stats' element={<Stats />} />
         </Route>
